28e48c39d031e72d1a32f8b95572fbf0b8efbe52
Derek Miller <derekmiller9@gmail.com>
2016-10-18 08:14:03 -0700

    Updated environment variables for mLab MongoDB

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const { MongoClient } = require('mongodb');
 const createApp = require('./create-app')
 
-const MONGO_URI = 'mongodb://localhost:27017/books';
-const PORT = 3000;
+const DEFAULT_MONGO_URI = 'mongodb://localhost:27017/books';
+const PORT = process.env.PORT || 3000
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const MONGODB_URI = process.env.MONGODB_URI || DEFAULT_MONGO_URI
 
 
-MongoClient.connect(MONGO_URI, (err, db) => {
+
+MongoClient.connect(MONGDBO_URI, (err, db) => {
   if (err) {
     console.error(err);
     process.exit(1);
@@ -14,6 +18,7 @@ MongoClient.connect(MONGO_URI, (err, db) => {
   const app = createApp(db);
 
   app.listen(PORT, () => {
+    NODE_ENV !== 'production' &&
     console.log('listening on port ' + PORT)
   })
 })
